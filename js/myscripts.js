3c63f59d8275fd9b161fbf78a266331da4f00b17
SebastianOcean <antnoz@hotmail.com>
2015-11-12 21:36:53 +0100

    themed, error detections, used SASS and Compass, other fixes

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 
+	"use strict"; 
 
   /////////////////////////// Declare general variables ///////////////////////////
 
 	var api_key,
-		img_base_url,	//the base url for the API's images
-		search_query,	//the current value of the search input 
+		img_base_url,		//the base url for the API's images
+		search_query,		//the current value of the search input 
 		$results_container,	//points to the html container where the results will be displayed 
 		pages = {
 			page_number : 1,
@@ -22,8 +23,8 @@ $(document).ready(function () {
 	// Self-executing anonymous function initializes general components
 
 		api_key = '42b5ff0dac4f85d35326ee0cdf58f291';
-
-		search_query = $('#search_input').val();
+		
+		$('#search_input').val('');
 
 		$results_container = $('#results_display');
 
@@ -32,6 +33,9 @@ $(document).ready(function () {
 			'api_key' : api_key
 		}, function(data){
 			img_base_url = data.images.base_url;
+		}).error(function(){
+			basic_list_transition();
+			$results_container.html('<li><small>Error connecting to themoviedb\'s API</small></li>');
 		});
 	})();
 
@@ -52,41 +56,45 @@ $(document).ready(function () {
 
 			//use the function passed as an argument to build de list
 			build_movie_list(query, data.results, data.total_results, pages);
+		}).error(function(){
+			basic_list_transition();
+			$results_container.html('<li><small>Error connecting to themoviedb\'s API</small></li>');
 		});
 	}
 
 
-	function build_basic_movie_list(query, sresults, total_results, pages){
-	//Builds the API response in the $results_container
+	function build_basic_movie_list(query, search_results, total_results, pages){
+	//Builds the API response in the $results_container, as a simple list
 
 		if (total_results > 0) {
 
-			var search_result = '';
+			var s_r = ''; //will handle the html output
 
-			$.each(sresults, function(i, movie) {
+			$.each(search_results, function(i, movie) {
 
-				search_result += '<li class="list_element">';
-				search_result += '<em class="movie_title">';
-				search_result += movie.original_title;
-				search_result += '</em>';
+				s_r += '<li class="list_element">';
+				s_r += '<em class="movie_title">';
+				s_r += movie.original_title;
+				s_r += '</em>';
 				if (movie.release_date!==null && movie.release_date!=="") 
-					{search_result += '<span class="year"> (' + movie.release_date.substring(0, 4) + ')</span>';}
-				search_result += '</li>';
-				$results_container.html(search_result);
+					{s_r += '<span class="year"> (' + movie.release_date.substring(0, 4) + ')</span>';}
+				s_r += '</li>';
+				$results_container.html(s_r);
 
 			});
 
-			$results_container.html(search_result);
+			$results_container.html(s_r);
 		} else {
-			$results_container.html('<li><small>No movie found for "' + query + '".</small></li>');
+			$results_container.html('<li> <small>No movie found for "' + query + '".</small></li>');
 		}
 	}
 
 	var build_extended_movie_list = function(query, search_results, total_results, pages){
+	//Builds the API response in the $results_container, as an enhanced list
 		if (total_results > 0) {			
 
-			var img_url;
-			var s_r = ''; //will handle the html output
+			var img_url,
+			 	s_r = ''; //will handle the html output
 
 			$.each(search_results, function(i, movie) {
 
@@ -115,11 +123,11 @@ $(document).ready(function () {
 				s_r += '<span id="pages">';
 				var page_n = parseInt(pages.page_number)-1;
 				if(page_n > 0){
-					s_r += '<a href="#" id="prev_page">(prev ' + page_n + '/' + pages.number_of_pages + ')</a>';
+					s_r += '<a href="#" id="prev_page">&#8592; ' + page_n + '/' + pages.number_of_pages + '</a>';
 				}
 				var page_n = parseInt(pages.page_number)+1;
 				if(page_n < pages.number_of_pages){
-					s_r += '<a href="#" id="next_page">(next ' + page_n + '/' + pages.number_of_pages + ')</a>';
+					s_r += '<a href="#" id="next_page">' + page_n + '/' + pages.number_of_pages + ' &#8594;</a>';
 				}
 				s_r += '<br/>Page ' + pages.page_number;				
 				s_r += '</span>';
@@ -127,34 +135,41 @@ $(document).ready(function () {
 
 			$results_container.html(s_r);
 		} else {
-			$results_container.html('<li> <small>No movie found for "' + query + '".</small></li>');
+			$results_container.html('<li><small>No movie found for "' + query + '".</small></li>');
 		}
 	}
 
 
+	//functions editing the css/html from one view to an other
 	function basic_list_transition(){
 		$('body').css({'overflow': 'hidden'});
 		$('#search_input').css({'border-bottom-left-radius': '0'});
 		$('#submit_button').css({'border-bottom-right-radius': '0'});
 		$('#results_display').css({'display': 'block'});
 
+		$('#results_display').css({'font-size': '1em'})				
 		$('#wrapper').css({'top': '40%'});
 		$('#results_display').css({'width': '240px'});
 		$('#search_form').css({'width': '240px'});
-		$('#search_input').css({'width': '180px'});
-
-
+		$('#search_input').css({'width': '153px'});
 	}
 
-		
 	function extended_list_transition(){
 		$('body').css({'overflow': 'auto'});
+		$('#results_display').css({'font-size': '1.2em'})
 		$('#wrapper').css({'top': '10px'});
 		$('#results_display').css({'width': '400px'});
 		$('#search_form').css({'width': '400px'});
-		$('#search_input').css({'width': '300px'});
-
-
+		$('#search_input').css({'width': '273px'});
+	}
+		
+	function reset_search_input(){
+		basic_list_transition();
+		$('#search_input').val('');
+		$('#results_display').html('');
+		$('#search_input').css({'border-bottom-left-radius': '5px'});
+		$('#submit_button').css({'border-bottom-right-radius': '5px'});
+		$('#search_input').focus();
 	}
 
 
@@ -170,18 +185,17 @@ $(document).ready(function () {
 	});
 
 	$('#clear_bttn').click(function(e){
+
+		$('#clear_bttn').mouseleave();
+
 		e.preventDefault();
-		$('#search_input').val('');
-		$('#results_display').html();
-		$('#search_input').focus();
-		$('#search_input').keyup();
-		basic_list_transition();
-		$('#search_input').css({'border-bottom-left-radius': '5px'});
-		$('#submit_button').css({'border-bottom-right-radius': '5px'});
-		$('#results_display').html('');
+
+		reset_search_input();
 	
 	});
 	
+
+	//blurs the results when a key is pressed, as a transition
 	$('#search_input').keypress(function(e){
 		$('#results_display li').css({'opacity': '0'});
 	});
@@ -200,7 +214,7 @@ $(document).ready(function () {
 			get_moviedb_result(search_query, build_basic_movie_list, pages);
 			
 		} else{
-			$('#results_display').html('<li><small>the results will be displayed here</small></li>');
+			reset_search_input();
 		}
 	});
 
@@ -214,13 +228,15 @@ $(document).ready(function () {
 		search_query = $('#search_input').val();
 
 		if (search_query !== ''){
-			
+			$('#submit_button').mouseout();
+
 			extended_list_transition();
 			pages.page_number = pages.number_of_pages = 1; 
 
 			get_moviedb_result(search_query, build_extended_movie_list, pages);
 			
 		} else{
+			basic_list_transition();
 			$('#results_display').html('<li><small>the results will be displayed here</small></li>');
 		}
 	});
@@ -277,8 +293,5 @@ $(document).ready(function () {
 		}
 
 	});
-	
-	
 
 });
-
